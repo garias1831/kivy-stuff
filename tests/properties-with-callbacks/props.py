d11b63ccf9c4e148415f57e6f6ddd5178e896582
Gabe <107259694+TrufflePig123@users.noreply.github.com>
2023-05-14 16:28:53 -0400

    Create props.py

---
+++ tests/properties-with-callbacks/props.py
@@ -0,0 +1,26 @@
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.properties import NumericProperty
+
+class MyButton(Button):
+    count = NumericProperty(0)
+
+    def on_count(self, instance, value):
+        print(f'Count changed to {value}')
+
+    '''
+    Note that in this example, we didn't explicitly bind the on_count method to the count property. 
+    Kivy automatically detects that there is a method with the correct name (on_count) and binds it to the property. 
+    If you have a different naming convention for your callback methods, you can use the ObjectProperty class to explicitly bind your callbacks to your properties.
+    '''
+
+    def on_release(self):
+        self.count += 1
+
+class MyApp(App):
+    def build(self):
+        button = MyButton(text='Click me!')
+        return button
+
+if __name__ == '__main__':
+    MyApp().run()
